d86e2fe8cd07027ce3ceafcfacb8dd59abbb6d86
Mareks Kalējs <M.Kalejs@outlook.com>
2019-01-11 16:20:44 +0100

    Added mqtt settings

---
+++ HomeSecurityAPI/Models/MqttSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeSecurityAPI.Models
+{
+    public class MqttSettings
+    {
+        [Required]
+        public string BrokerHostName { get; set; }
+
+        [Required]
+        public uint Port { get; set; }
+
+        [Required]
+        public bool UseSecureConnection { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+    }
+}
---
+++ HomeSecurityAPI/Services/MqttService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace HomeSecurityAPI.Services
+{
+    public class MqttService
+    {
+        public MqttService()
+        {
+            //var settings = AppSettingsProvider.Create<MqttSettings>();
+            //var port = Convert.ToInt32(settings.Port);
+
+            //MqttClient client = null;
+        }
+    }
+}
